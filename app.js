071b41b432110270e59daf8c97e681f8a7617c3c
cdkrieg <cdkrieg@outlook.com>
2022-02-28 18:58:57 -0600

    added function to capitalize first letter of words and return a string

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ alert(stringReversed);
 //Capitalize a letter
 function splitStringIntoWords(string){
     let myArrayOfWords = string.split(" ");
+    console.log(myArrayOfWords);
+    return myArrayOfWords;
 }
+function capitalizeFirstLetterOfWord(array){
+    let newString = "";
+    for(let i = 0; i < array.length; i++){
+        newString = newString + array[i].charAt(0).toUpperCase() + array[i].slice(1) + " ";
+    }
+
+    return newString;
+}
+let userInput = prompt("enter a sentence")
+let splitString = splitStringIntoWords(userInput);
+let capitalizedWords = capitalizeFirstLetterOfWord(splitString);
+alert(capitalizedWords);
+
