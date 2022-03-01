3b690d180f446654f595cc77cc2ec675b6a5a70f
cdkrieg <cdkrieg@outlook.com>
2022-02-28 18:27:47 -0600

    created index.html and app.js, added function to reverse a string (not tested)

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+//Reverse a string
+function reverseString(string){
+    let stringArray = [];
+    let newString = "";
+    for(let ){
+        stringArray.push(char);
+    }
+    for(let i = stringArray.length-1; i>=0; i-- ){
+        let newString = "";
+        newString = newString + stringArray[i];
+    }
+    return newString;
+}
+let userString = prompt("Enter a string to reverse");
+let stringReversed = reverseString(userString);
+alert(stringReversed);
